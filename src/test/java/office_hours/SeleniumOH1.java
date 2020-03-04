f6b4a6670945df2b764e07005ae34d50934596a1
RoksanaBo <roksana85@hotmail.com>
2020-03-04 14:46:25 -0600

    created basic navigation class

--- src/test/java/office_hours/SeleniumOH1.java
+++ src/test/java/office_hours/SeleniumOH1.java
@@ -18,12 +18,10 @@ public class SeleniumOH1 {
             - List, Set, Map, and other types of collections can use Iterator class to iterate over that collection
             - List has indexes (for i, for each, while, Iterator)
             - Map (for each, Iterator - to loop through keys)
-        - Basic Selenium Navigations -
+        - Basic Selenium Navigation -
      */
 
-
-
-            // Create arrayList of Strings - iterate over it
+    // Create arrayList of Strings - iterate over it
 
             ArrayList<String> keysToSearch = new ArrayList<>();
             keysToSearch.add("fruits");
@@ -49,6 +47,7 @@ public class SeleniumOH1 {
 
             //create a map with <String, String>
             HashMap<String, String> personalInfo = new HashMap<>();
+
             personalInfo.put("name", "Bryan");
             personalInfo.put("student_id","2314635541" );
             personalInfo.put("major", "computer science");
@@ -59,20 +58,25 @@ public class SeleniumOH1 {
                 System.out.println(key + ":" + personalInfo.get(key));
             }
 
-            //========================
+            //=================================================================
             //SELENIUM
         /*
             id - unique (it is not always available) - we always want to use it when it is available
             class - classname
             name
             tag - every element will have a tag
-            will only work with the link
-            linktext
-            partial linktext
+
+
+            linkText   will only work with the link
+            partial linkText
+
+
             locator that is using html (syntax)
             css
             xpath
-            --------------
+            ------------------------------------------------------------------------------------------
+
+
             <input id="global-enhancements-search-query" data-id="search-query"
             data-global-enhancements-search-input=""
             type="text" name="search_query"
@@ -80,28 +84,36 @@ public class SeleniumOH1 {
             global-enhancements-search-input-btn-group__input wt-pr-xs-7"
             placeholder="Search for items or shops" value="handbags"
             autocomplete="off" autocorrect="off" autocapitalize="off">
+
+
             key = "value" - attributes
             input - tag
+
             id="global-enhancements-search-query" - one of the attributes
             we can use id to locate the element
+
             name="search_query" - attribute - we can use name to locate the element
+
             class="wt-input wt-input-btn-group__input
+               - we can use class to locate element
+
             global-enhancements-search-input-btn-group__input wt-pr-xs-7"
-            - we can use class to locate element
+
             When we want to find element it is always must be unique
+
             id- always unique
             name, tag, class - are not unique very often
          */
 
             WebDriverManager.chromedriver().setup();
-
             WebDriver driver = new ChromeDriver();
 
             driver.get("http://etsy.com");
 
             WebElement searchBar = driver.findElement(By.id("global-enhancements-search-query"));
+
             //findElement() -> returns WebElement
-            //as param we put By.locator("value of attribute")
+            //as parameter we put By.locator("value of attribute")
             //                  (id, name, classname, class, ....)
 
 
