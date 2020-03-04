8a3740a79ea5bb02540c13ac6124c082115e0970
RoksanaBo <roksana85@hotmail.com>
2020-03-04 11:48:51 -0600

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -19,12 +19,16 @@ public class BasicNavigation {
 
         driver.get("http://google.com"); // to open the web site
 
+        driver.manage().window().maximize(); // to maximize screen
+
+        // driver.manage().window().fullscreen(); // full screen
+
         Thread.sleep(2000); // for demo - wait 2 seconds
 
 
         String title = driver.getTitle(); // returns <title>Some title</title> text
 
-        String expectedTitle = "Google";
+        String expectedTitle = "Google"; // we provide it
 
         System.out.println("Title is : " + title);
         //this method returns page title
@@ -35,16 +39,58 @@ public class BasicNavigation {
         }else
             System.out.println("test failed");
 
+
+
+        // go to another website within the same window
+        driver.navigate().to("http://amazon.com");
+
+        if(driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("TEST PASSED");
+        }else{
+            System.out.println("TEST FAILED");
+        }
+
+
+
+
+        driver.navigate().back();
+
+
+        Thread.sleep(2000);
+        // checking if page title equals to Google
+        verifyEquals(driver.getTitle(),"Google");
+
+
+        //move forward in the browser history:
+        driver.navigate().forward();
+
+        Thread.sleep(2000);
+        System.out.println("title " + driver.getTitle());
+
+
+        System.out.println("URL : " + driver.getCurrentUrl()); // to get URL
+
+
+        driver.navigate().refresh(); // to reload page
+        Thread.sleep(2000);
+
+
+
         driver.close(); // to close browser, browser cannot close itself.
 
 
+    }
 
 
 
-
-
-
-
+    public static void verifyEquals(String arg1, String arg2){
+        if(arg1.equals(arg2)){
+            System.out.println("test passed");
+        }else{
+            System.out.println("test failed");
+        }
 
     }
+
+
 }
---
+++ src/test/java/com/automation/tests/day2/JumpToNewWindow.java
@@ -0,0 +1,13 @@
+package com.automation.tests.day2;
+
+public class JumpToNewWindow {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
