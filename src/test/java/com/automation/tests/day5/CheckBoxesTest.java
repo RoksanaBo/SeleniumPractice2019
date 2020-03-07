02f2e91fa942e01c32bb4b11184fbbf797c5e670
RoksanaBo <roksana85@hotmail.com>
2020-03-07 14:37:06 -0600

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day5/CheckBoxesTest.java
@@ -0,0 +1,28 @@
+package com.automation.tests.day5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckBoxesTest {
+    public static void main(String[] args) {
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        // Task:
+        // verify that first checkbox is not
+        // selected and second is selected:
+
+
+
+
+
+
+
+
+
+
+    }
+}
