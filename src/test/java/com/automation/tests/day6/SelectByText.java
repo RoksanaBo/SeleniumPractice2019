cbd2fb1d6fe16f627ca047c65b798f52b3fb8b71
RoksanaBo <roksana85@hotmail.com>
2020-03-08 09:37:42 -0600

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day6/SelectByText.java
@@ -0,0 +1,56 @@
+package com.automation.tests.day6;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SelectByText {
+
+    public static void main(String[] args) {
+
+        //    WebDriverManager.chromedriver().setup();
+        //    WebDriver driver = new ChromeDriver();
+
+        WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        BrowserUtils.wait(3);
+
+        // create webElement object or dropdown:
+        WebElement simpleDropdown = driver.findElement(By.id("dropdown"));
+
+        // provide webElement object into constructor:
+        Select selectSimpleDropdown = new Select(simpleDropdown);
+
+        // select by visible text:
+        selectSimpleDropdown.selectByVisibleText("Option 2");
+
+
+
+
+
+
+
+
+
+
+
+
+        BrowserUtils.wait(3);
+        driver.quit();
+
+
+
+
+
+
+
+
+
+
+    }
+}
