04d5e01f178240f4e04011815dbfdb109bb5a05f
RoksanaBo <roksana85@hotmail.com>
2020-03-14 14:39:41 -0600

    testNG

---
+++ src/test/java/com/automation/tests/day8/PracticeTest.java
@@ -0,0 +1,71 @@
+package com.automation.tests.day8;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+
+    private WebDriver driver;
+
+
+    /**
+     * we put test annotation to make methods
+     * executable as tests.
+     */
+
+    @Test
+    public void loginTest(){
+        driver.findElement(By.linkText("Form Authentication")).click();
+        BrowserUtils.wait(3);
+        driver.findElement(By.name("username")).sendKeys("tomsmith");
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword", Keys.ENTER);
+
+        BrowserUtils.wait(3);
+        String expected = "Welcome to the Secure Area. When you are done click logout below.";
+        String actual = driver.findElement(By.className("subheader")).getText();
+
+        // if assertion fails, it will throe exception and message will be printed
+        // 3 parameters : (expected, actual, message in case of error)
+        Assert.assertEquals(actual,expected,"Sub-header message is not matching");
+
+    }
+
+
+
+    @BeforeMethod
+    public void setup(){
+        WebDriverManager.chromedriver().version("79").setup();
+
+        // HOW TO HANDLE SSL ISSUES IN SELENIUM?
+        //  ChromeOptions - use to customize browser for tests
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        // to ignore "Your connection is not private issue
+
+        chromeOptions.setAcceptInsecureCerts(true);
+
+        // provide chromeOptions object into chromeDriver constructor
+
+        driver =new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/");
+        driver.manage().window().maximize();
+    }
+
+
+    @AfterMethod
+    public void teardown(){
+        driver.quit();
+    }
+
+
+}
