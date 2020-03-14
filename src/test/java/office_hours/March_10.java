5eade37be7871e77eceae9321fffaf0e494646fa
RoksanaBo <roksana85@hotmail.com>
2020-03-14 09:49:35 -0600

    created basic navigation class

---
+++ src/test/java/office_hours/March_10.java
@@ -0,0 +1,35 @@
+package office_hours;
+
+public class March_10 {
+
+    public static void main(String[] args) {
+
+
+/*
+     Under List Interface:
+     - ArrayList (indexes, faster to read, slower when we
+       do manipulations(remove or add elements)
+     - Vector (same as ArrayList, but thread safe)
+     - Stack (Last in First out (like a stack of paper))
+     - LinkedList (node (each element reference to previous
+       and next element in the list))
+       (performance- sensitive чувствительный к производительности,
+       memory allocation  выделение памяти)
+
+
+      [2, 5, 3] --> [2, 3]
+      [2] [5] [3]
+
+
+      Set Interface: contains only unique values
+      -HashSet -->
+      -TreeSet
+      -LinkedHashSet
+
+ */
+
+
+
+
+    }
+}
