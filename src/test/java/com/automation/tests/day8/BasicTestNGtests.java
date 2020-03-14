9c4774466a5e5319bc084ccafc3ac9e4c6b46239
RoksanaBo <roksana85@hotmail.com>
2020-03-14 11:14:49 -0600

    testNG

--- src/test/java/com/automation/tests/day8/BasicTestNGtests.java
+++ src/test/java/com/automation/tests/day8/BasicTestNGtests.java
@@ -1,13 +1,25 @@
 package com.automation.tests.day8;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BasicTestNGtests {
 
 
+
+    @BeforeMethod     // runs before every test automatically, works as a pre-condition or setup.
+    public void setup(){
+        System.out.println("Before Method");
+
+    }
+
+
+
     @Test
     public void test1(){
+        System.out.println("Test 1");
+
         String expected = "apple";
         String actual = "apple";
         Assert.assertEquals(actual,expected);
@@ -17,12 +29,14 @@ public class BasicTestNGtests {
 
     @Test
     public void test2(){
+        System.out.println("Test 2");
+
         int num1 = 5;
         int num2 = 10;
 
         // it called hard assertion
         // if assertion fails - it stops the execution(due to exception)
-        Assert.assertTrue(num1 > num2);
+        Assert.assertTrue(num1 < num2);
 
     }
 }
