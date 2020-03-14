b4a7a72fa6a10f17ef28e65de3a7beb2561d3ccf
RoksanaBo <roksana85@hotmail.com>
2020-03-14 15:58:47 -0600

    testNG

--- src/test/java/com/automation/tests/day8/PracticeTest.java
+++ src/test/java/com/automation/tests/day8/PracticeTest.java
@@ -5,12 +5,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class PracticeTest {
 
@@ -68,6 +71,22 @@ public class PracticeTest {
 
 
 
+    @Test
+    public void checkBoxTest1(){
+        driver.findElement(By.linkText("Checkboxes")).click();
+
+        // locator for specific checkbox, xpath: // input[1]     cssSelector: input:nth-of-type(1)
+        // collect all checkboxes
+        List<WebElement> checkboxes = driver.findElements(By.tagName("input"));
+        BrowserUtils.wait(3);
+
+        checkboxes.get(0).click(); // to click on checkbox #1
+
+        Assert.assertTrue(checkboxes.get(0).isSelected(),"Checkbox 1 is not selected");
+
+        BrowserUtils.wait(2);
+
+    }
 
 
 
