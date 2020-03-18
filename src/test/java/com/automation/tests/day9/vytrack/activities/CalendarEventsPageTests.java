8b1f38fae0745d32e3504ed9311b001560878f7a
RoksanaBo <roksana85@hotmail.com>
2020-03-17 18:10:37 -0600

    Calls Page Test

---
+++ src/test/java/com/automation/tests/day9/vytrack/activities/CalendarEventsPageTests.java
@@ -0,0 +1,15 @@
+package com.automation.tests.day9.vytrack.activities;
+
+public class CalendarEventsPageTests {
+
+
+
+    //   Scenario 2 : Verify for store manager
+    //   Login as store manager
+    //   Go to Activities --> Calendar Events
+    //   Verify that Create Calendar Event button is displayed
+
+
+
+
+}
--- src/test/java/com/automation/tests/day9/vytrack/activities/CallsPageTest.java
+++ src/test/java/com/automation/tests/day9/vytrack/activities/CallsPageTest.java
@@ -82,30 +82,4 @@ public class CallsPageTest {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- //   Scenario 2 : Verify for store manager
- //   Login as store manager
- //   Go to Activities --> Calendar Events
- //   Verify that Create Calendar Event button is displayed
-
-
-
-
-
-
 }
