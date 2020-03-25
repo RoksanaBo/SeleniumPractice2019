c2e5fc9b6ae4e2ecb126cf2da2fe9ab17e8eda32
RoksanaBo <roksana85@hotmail.com>
2020-03-25 14:49:17 -0600

    selenium

--- src/test/java/com/automation/tests/d2/BasicNavigation.java
+++ src/test/java/com/automation/tests/d2/BasicNavigation.java
@@ -39,7 +39,7 @@ public class BasicNavigation {
         }else
             System.out.println("test failed");
 
-
+//------------------------------------------------------------------------------------------------------------------------
 
         // go to another website within the same window
         driver.navigate().to("http://amazon.com");
