fe75fabfc56e282661e13257e83d8c9176c72c5e
RoksanaBo <roksana85@hotmail.com>
2020-03-25 18:44:44 -0600

    driver factory

--- src/test/java/com/automation/tests/d3_March3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/d3_March3/FindElementsPractice.java
@@ -15,6 +15,9 @@ public class FindElementsPractice {
         // test package -this is where we gonna store all our tests.
         // utilities - package for storing reusable methods that will support our tests.
         // for example - we can create a class with the method that will give us a WebDriver whenever we need it.
+
+
+
         WebDriver driver = DriverFactory.createDriver("chrome");
 
         driver.get("http://practice.cybertekschool.com/sign_up");
@@ -22,18 +25,19 @@ public class FindElementsPractice {
         WebElement fullName = driver.findElement(By.name("full_name"));
         fullName.sendKeys("Mister Twister");
 
-        Thread.sleep(2000);
+        Thread.sleep(3000);
 
         WebElement email = driver.findElement(By.name("email"));
         email.sendKeys("sdet@cybertek.com");
 
-        Thread.sleep(2000);
+        Thread.sleep(3000);
 
         WebElement signUp = driver.findElement(By.name("wooden_spoon"));
 
         //when you see type="submit", you can use submit() instead of click()
         //it make sense to use when click() method doesn't work
-        signUp.submit();
+
+        signUp.submit(); // or signUp.click();
         Thread.sleep(2000);
 
         String expected = "Thank you for signing up. Click the button below to return to the home page.";
