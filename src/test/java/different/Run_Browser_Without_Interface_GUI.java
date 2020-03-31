732c6a01d0d1079e3afd6e7f383f917fcce7841e
RoksanaBo <roksana85@hotmail.com>
2020-03-31 12:37:26 -0600

    BrowserUtils

---
+++ src/test/java/different/Run_Browser_Without_Interface_GUI.java
@@ -0,0 +1,24 @@
+package different;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Run_Browser_Without_Interface_GUI {
+private WebDriver driver;
+
+    public void setup() {
+
+        //  how to run browser without GUI , Makes browser invisible. (Interface)
+        WebDriverManager.chromedriver().version("79").setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setHeadless(true);
+        driver = new ChromeDriver(chromeOptions);
+
+        // headless mode makes execution twice faster
+        // it does everything except file uploading
+        // set it to true to make it work
+
+    }
+}
