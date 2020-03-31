5406cf8308a706376c84b34c02e3d0695c98eb14
RoksanaBo <roksana85@hotmail.com>
2020-03-31 16:01:00 -0600

    explicit wait test

--- src/test/java/com/automation/tests/d13_March22/ExplicitWait.java
+++ src/test/java/com/automation/tests/d13_March22/ExplicitWait.java
@@ -1,10 +1,13 @@
 package com.automation.tests.d13_March22;
 
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -23,8 +26,43 @@ public class ExplicitWait {
 
         WebDriverWait wait = new WebDriverWait(driver,10);
         // wait up to 10 seconds until title contains "Google"
-        wait.until(ExpectedConditions.titleContains("Google"));
+        wait.until(ExpectedConditions.titleContains("Google"));// it confirms that title is Google
+
+        // we can also navigate to amazon
+        // and verify if title contains Amazon
+        driver.navigate().to("https://amazon.com");
+        wait.until(ExpectedConditions.titleContains("Amazon"));
     }
+
+
+    @Test
+    public void waitForVisibility(){
+        driver.get("http://practice.cybertekschool.com/dynamic_loading/1");
+
+        WebDriverWait wait = new WebDriverWait(driver,10);
+
+        // click on start button:
+        driver.findElement(By.xpath("//button[text()='Start']")).click();
+
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        wait.until(ExpectedConditions.visibilityOf(username)).sendKeys("tomsmith");
+        wait.until(ExpectedConditions.visibilityOf(password)).sendKeys("SuperSecretPassword");
+        wait.until(ExpectedConditions.visibilityOf(submitBtn));
+
+        wait.until(ExpectedConditions.elementToBeClickable(submitBtn)).click();
+
+
+        // Assertions:
+        String expected = "Welcome to the Secure Area. When you are done click logout below.";
+        String actual = driver.findElement(By.className("subheader")).getText();
+
+        Assert.assertEquals(actual,expected);
+
+    }
+
 
     @AfterMethod
     public void teardown(){
